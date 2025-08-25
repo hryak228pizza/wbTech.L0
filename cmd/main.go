f793a25ac9850c291d0c18999168e4df3abd9fb5
hryak228pizza <I.Pavel-2004@mail.ru>
2025-08-26 02:19:19 +0300

    add documentation

--- cmd/main.go
+++ cmd/main.go
@@ -8,16 +8,25 @@ import (
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 
+	"github.com/hryak228pizza/wbTech.L0/internal/logger"
+	h "github.com/hryak228pizza/wbTech.L0/internal/transport/handler"
 	c "github.com/hryak228pizza/wbTech.L0/internal/transport/kafka/consumer"
 	p "github.com/hryak228pizza/wbTech.L0/internal/transport/kafka/producer"
-	h "github.com/hryak228pizza/wbTech.L0/internal/transport/http"
-	"github.com/hryak228pizza/wbTech.L0/internal/logger"
+    _ "github.com/hryak228pizza/wbTech.L0/internal/transport/handler/docs"
+	"github.com/swaggo/http-swagger"
+	"github.com/hryak228pizza/wbTech.L0/pkg/cache"
 	"go.uber.org/zap"
 )
 
+//	@title			Order service API
+//	@version		1.0
+//	@description	Service for processing, storing and displaying order data
+
+//	@host		localhost:8080
+//	@BasePath	/
 
 func main() {
-
+	
 	// first initialization of logger
 	logger.Logger()
     defer logger.L().Sync()
@@ -29,17 +38,27 @@ func main() {
         logger.L().Info("failed to open database")
     }     
     defer db.Close()
+
+	// create cache
+	lru, err := cache.NewCache(5, db)
+	if err != nil {
+		logger.L().Fatal("failed to create cache", 
+			zap.String("error", err.Error()),
+		)
+	}
         
 	// handlers setup
     handlers := &h.Handler{
-		DB:   db,
-		Tmpl: template.Must(template.ParseGlob("templates/*")),
+		DB:    db,
+		Tmpl:  template.Must(template.ParseGlob("templates/*")),
+		Cache: lru,
 	}
 
 	// create router 
     r := mux.NewRouter()
     r.HandleFunc("/", handlers.Page).Methods("GET")
-	r.HandleFunc("/order/{id}", handlers.List).Methods("GET")
+	r.HandleFunc("/order/{id}", handlers.List).Methods("GET")	
+	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
     logger.L().Info("starting server",
 		zap.String("host", "localhost"),
